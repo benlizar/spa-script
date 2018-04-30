17fd4cb765de5a3c8738011224e52f194d90dd43
Rafael Noguera <rafanog2155@gmail.com>
2018-04-30 10:38:24 -0400

    Update lock_login_test.js

--- lock_login_test.js
+++ lock_login_test.js
@@ -4,10 +4,10 @@ Scenario('Log in using Lock', (I) => {
     I.amOnPage('/');
     I.waitForVisible('#qsLoginBtn', 3);
     I.click('#qsLoginBtn');
-    I.waitForVisible('input[name="password"]', 10);
+    I.waitForVisible('input[name="password"]', 20);
     I.wait(1);
     I.fillField('input[name="email"]', 'prueba');
     I.fillField('input[name="password"]', 'prueba');
     I.click('.auth0-lock-widget-container .auth0-lock-submit');
-    I.retry(2).waitForVisible('#qsLogoutBtn', 5);
+    I.retry(2).waitForVisible('#qsLogoutBtn', 20);
 });
